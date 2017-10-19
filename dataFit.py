5c00080d81e51529884baab1b507554859136c5d
Your Name <you@example.com>
2017-10-19 03:10:46 +0000

    Updated missing data fill twith padded arrays

--- dataFit.py
+++ dataFit.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import time
 
-from scipy import optimize as op
-from scipy import fftpack as ft
-from scipy import signal as sg
+import scipy.optimize as op
+import scipy.fftpack as ft
+import scipy.signal as sg
+import scipy.ndimage.filters as flt
 
 def date2epoch(date):
     pattern = "%Y-%m-%d"
@@ -30,7 +31,7 @@ def fitCurve(func, xdata, ydata):
 
 def fitSine(func, xdata, ydata):
     sampRate = 1/(xdata[1]-xdata[0])
-    spect, freqs = mlab.psd(ydata, Fs=sampRate, detrend='linear', NFFT=16, noverlap=4)
+    spect, freqs = mlab.psd(ydata, Fs=sampRate, detrend='linear', NFFT=16, noverlap=8)
 #    spect = ft.dct(ydata)
 #    idx = np.argmax( np.abs(spect[1:]) ) + 1
 #    freq = 1/(xdata[1] - xdata[0]) * 2 * (idx)/len(ydata)
@@ -39,6 +40,7 @@ def fitSine(func, xdata, ydata):
     return fitCurve(func, xdata * maxFreq, ydata)
 
 def interpolateMissing(data, r=1):
+    data = np.pad(data, r, mode='reflect')
     mask = np.isnan(data)
     buf = np.zeros_like(data[mask])
     newMask = np.array(mask)
@@ -48,19 +50,33 @@ def interpolateMissing(data, r=1):
         buf = buf + np.nan_to_num(temp)
         newMask = newMask & np.roll(newMask, 1)
     data[mask] = buf
-    if newMask.any():
+    data = data[r:-r]
+    if newMask[r:-r].any():
         for i in range(r):
             newMask = newMask | np.roll(newMask, -1)
-        data[newMask] = float('nan')
+        data[newMask[r:-r]] = float('nan')
         data = interpolateMissing(data, r+1)
     return data
-    
-dict = {0: date2epoch, 1: missing2nan, 2: missing2nan, 3: missing2nan, 4: missing2nan, 5: missing2nan, 6: missing2nan}
+
+def stripOutliers(data):
+#    median = sg.medfilt(data, 5)
+    median = flt.median_filter(data, size=5, mode='mirror')
+    diff = abs(data - median)
+#    mad = sg.medfilt(diff, 5)
+    mad = flt.median_filter(diff, size=5, mode='mirror')
+    mask = diff > (4 * mad)
+    data[mask] = float('nan')
+    return interpolateMissing(data)
+
 def loadData(ticker):
+    dict = {0: date2epoch, 1: missing2nan, 2: missing2nan, 3: missing2nan, 4: missing2nan, 5: missing2nan, 6: missing2nan}
     blob = np.loadtxt(ticker + '.txt', skiprows=1, delimiter=',', converters=dict, unpack=True)
     for idx, col in enumerate(blob):
-        if np.isnan(col).any():
-            blob[idx] = interpolateMissing(col)
+        if not idx: continue
+        data = np.array(col)
+        if np.isnan(data).any():
+            data = interpolateMissing(data)
+        blob[idx] = stripOutliers(data)
     return blob
 
 
