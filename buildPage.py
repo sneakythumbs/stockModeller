b686c4eb752be0816d0f0d287871119b5ce43327
Your Name <you@example.com>
2017-11-17 03:18:33 +0000

    added html formatting in preparation for displaying summary statistics

--- buildPage.py
+++ buildPage.py
@@ -5,11 +5,14 @@ from dominate.tags import *
 
 def writeWebPage(fullpath):
     images = [os.path.basename(x) for x in reversed(glob.glob(fullpath + '/*.svg'))]
+    data = [name.strip('.svg') + '.dat' for name in images]
 
     with document(title='') as doc:
         h1('')
         for path in images:
-            span(img(src=path), _class='image')
+            item = span(_class='plot', style="border:1px solid gray; display:inline-block;")
+            item.add(img(src=path))
+            item.add(pre(path.strip('.svg')))
 
     with open(fullpath +'/index.html', 'w') as f:
         f.write(doc.render())
